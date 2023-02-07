9284886ac439ce243fdab39cd70502db08f37d2a
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-02-07 20:25:33 +0100

    Add chulk and messing around with colors

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,14 @@
-const validator = require('validator');
+// const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes('Your notes...');
-
+const msg = chalk.underline(getNotes('Your notes...'));
 console.log(msg);
 
-console.log(validator.isURL('https/mead.io'));
+const greenMsg = chalk.green.inverse.bold('Success!');
+console.log(greenMsg);
+
+// console.log(validator.isURL('https/mead.io'));
 
 // const {firstName, add} = require('./utils.js');
 // const sum = add(4, -2);
