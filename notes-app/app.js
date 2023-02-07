d594fc700dbe167976490cddcc251351e64b3e30
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-02-07 20:52:02 +0100

    Getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,14 +2,26 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = chalk.underline(getNotes('Your notes...'));
-console.log(msg);
+const command = process.argv[2];
 
-const greenMsg = chalk.red.inverse.bold('Error!');
-console.log(greenMsg);
+console.log(process.argv);
 
+if (command === 'add') {
+    console.log('Adding note!');
+} else if (command === 'remove') {
+    console.log('Removing note!');
+};
+
+
+
+
+
+// const msg = chalk.underline(getNotes('Your notes...'));
+// console.log(msg);
+// const greenMsg = chalk.red.inverse.bold('Error!');
+// console.log(greenMsg);
+// console.log(process.argv[2]);
 // console.log(validator.isURL('https/mead.io'));
-
 // const {firstName, add} = require('./utils.js');
 // const sum = add(4, -2);
 // console.log(firstName);
