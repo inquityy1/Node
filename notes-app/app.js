3ac6fe515bef64470f85f89223781d8e0fb8275d
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-02-08 20:45:29 +0100

    Add yargs and using it for commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,28 +1,44 @@
-// const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!');
+    },
+});
 
-if (command === 'add') {
-    console.log('Adding note!');
-} else if (command === 'remove') {
-    console.log('Removing note!');
-};
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    },
+});
 
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Lis your notes',
+    handler: function () {
+        console.log('Listing out all note');
+    },
+});
 
+// Create list read
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    },
+});
 
-
-
-// const msg = chalk.underline(getNotes('Your notes...'));
-// console.log(msg);
-// const greenMsg = chalk.red.inverse.bold('Error!');
-// console.log(greenMsg);
-// console.log(process.argv[2]);
-// console.log(validator.isURL('https/mead.io'));
-// const {firstName, add} = require('./utils.js');
-// const sum = add(4, -2);
-// console.log(firstName);
-// console.log(sum);
+console.log(yargs.argv);
