2db54f821d793c090270eea05e4f4292ec6e9fd5
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-02-14 22:30:29 +0100

    Add request lib and learning how to make HTTP requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 Second Timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=975a64c1a6b2109d958bd091fe426631&query=37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('0 Second Timer');
-}, 0);
-
-console.log('Stoping');
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+});
