06077894e80e8c0fba641acd76642fd13372a81e
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-02-20 22:09:25 +0100

    Callback abstraction challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast')
 
 // const url = 'http://api.weatherstack.com/current?access_key=975a64c1a6b2109d958bd091fe426631&query=37.8267,-122.4233&units=f';
 
@@ -13,24 +14,22 @@ const geocode = require('./utils/geocode');
 //     }
 // });
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCjhIjoiY2pzbml20G92MGN2MTQ0cGd3bjVheTFuayJ9.hbZJE6qEZHEsL5QXVF4vtw&limit=1';
-
-// request({url: geocodeURL, json: true}, (error, response) => {
-//     if(error) {
-//         console.log('Unable to connect to location services!');
-//     } else if (response.body.features.length === 0) {
-//         console.log('No matching resaults');
-//     } else {
-//         const latitude = response.body.features[0].center[1];
-//         const longitude = response.body.features[0].center[0];
-//         console.log(latitude, longitude);
-//     }
+// geocode('New York', (error, data) => {
+//     console.log('Error', error);
+//     console.log('Data', data);
 // });
 
-geocode('New York', (error, data) => {
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(37.8267,-122.4233, (error, data) => {
     console.log('Error', error);
     console.log('Data', data);
 });
