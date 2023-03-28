3fea49755d5df150eae32c8825bbd69e655ab86d
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-03-28 20:35:02 +0200

    Serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,35 +1,18 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>');
+app.use(express.static(publicDirectoryPath));
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
 });
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Nebojsa',
-        age: 21,
-    }, {
-        name: 'Nemanja',
-    }]);
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-});
-
-app.get('/weather', (req, res) => {
-    res.send({
-           forecast: 'It is snowing',
-           location: 'Philadelphia', 
-        });
-});
-
-// app.com
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+  console.log("Server is up on port 3000.");
 });
