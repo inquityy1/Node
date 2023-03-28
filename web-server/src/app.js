edf3953806b22e2a16668edb9bb3cf08fcb5fbeb
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-03-28 22:39:28 +0200

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Nebojsa Nikolic'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Nebojsa Nikolic'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'Help',
+    name: 'Nebojsa Nikolic'
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
