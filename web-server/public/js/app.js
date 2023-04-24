0d304eea3da1f38de357b02cde57c4e261b1e7be
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-04-24 15:41:25 +0200

    Browser http request with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,23 @@
 console.log("Client side javascript file is loaded!");
+
+// CORS problem :/
+// fetch('http://puzzle.mead.io/puzzle', {
+//     mode: 'no-cors',
+//     headers: {
+//          "Content-Type": "application/json"
+//     },
+// }).then((resonse) => {
+//     resonse.json().then((data) => {
+//         console.log(data);
+//     });
+// });
+
+fetch('http://localhost:3000/weather').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data);
+        }
+    });
+});
