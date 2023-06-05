2f444fc63c41e1534983bc8f99c262c8ccb1d8fd
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-06-05 21:57:05 +0200

    Build post submission

--- 1.mini-microservice-app-grider/blog/client/src/App.js
+++ 1.mini-microservice-app-grider/blog/client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { PostCreate } from "./PostCreate";
 
-export default () => {
-  return <div>Blog app</div>;
+export const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
