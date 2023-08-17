bc7b6cdf23879187f6a025ac7b02a1f8f5cba9d1
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-08-17 17:59:39 +0200

    Restoring my k8s and making everything fork again and add test and routes file for tickets

--- 1.mini-microservice-app-grider/ticketing/tickets/src/index.ts
+++ 1.mini-microservice-app-grider/ticketing/tickets/src/index.ts
@@ -8,7 +8,10 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI must be defined");
+    }
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongodb");
   } catch (err) {
     console.error(err);
