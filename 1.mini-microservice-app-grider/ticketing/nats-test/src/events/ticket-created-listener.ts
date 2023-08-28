45dfd96d537d00384d58a99a5750776ec62264ca
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-08-28 20:31:44 +0200

    Add subjects and ticket-created-event

--- 1.mini-microservice-app-grider/ticketing/nats-test/src/events/ticket-created-listener.ts
+++ 1.mini-microservice-app-grider/ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -4,8 +4,12 @@ import { Listener } from "./base-listener";
 export class TicketCreatedListener extends Listener {
   subject = "ticket:created";
   queueGroupName = "payments-service";
+
   onMessage(data: any, msg: Message) {
     console.log("Event data!", data);
+
+    console.log(data.name);
+    console.log(data.cost);
 
     msg.ack();
   }
