7c13c38919a52ce6a13c757c0c1d37596642c44c
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-08-31 21:28:21 +0200

    Accessing the NATS client

--- 1.mini-microservice-app-grider/ticketing/tickets/src/index.ts
+++ 1.mini-microservice-app-grider/ticketing/tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,6 +11,7 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI must be defined");
     }
+    await natsWrapper.connect("ticketing", "laskjf", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongodb");
   } catch (err) {
