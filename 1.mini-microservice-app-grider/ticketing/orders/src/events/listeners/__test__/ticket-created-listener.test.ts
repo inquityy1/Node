1ceb3e023210c413ee6af74e88b9a85a67f8f69c
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-13 21:06:47 +0200

    Testing the ack call

--- 1.mini-microservice-app-grider/ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ 1.mini-microservice-app-grider/ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -37,12 +37,16 @@ it("creates and save a ticket", async () => {
   const ticket = await Ticket.findById(data.id);
 
   expect(ticket).toBeDefined();
-  expect(ticket!.title).toEqual("dsadsa");
+  expect(ticket!.title).toEqual(data.title);
   expect(ticket!.price).toEqual(data.price);
 });
 
 it("acks the message", async () => {
-  //   const { listener, data, msg } = await setup();
+  const { listener, data, msg } = await setup();
+
   // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
   // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
 });
