fe9137cfbfe14c5616966921a063d79cf3d1516d
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-13 19:33:28 +0200

    Planing test for ticket-create-listener

---
+++ 1.mini-microservice-app-grider/ticketing/orders/src/events/listeners/__test__/ticket-create-listener.test.ts
@@ -0,0 +1,15 @@
+const setup = async () => {
+  // create an istance of the listener
+  // create a fake data event
+  // create a fake message object
+};
+
+it("creates and save a ticket", async () => {
+  // call the onMessage function with the data object + message object
+  // write assertions to make sure a ticket was created
+});
+
+it("acks the message", async () => {
+  // call the onMessage function with the data object + message object
+  // write assertions to make sure ack function is called
+});
