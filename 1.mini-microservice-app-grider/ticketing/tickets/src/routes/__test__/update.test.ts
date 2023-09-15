72390529bb378de622c6fa79fd32fe227dab9596
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-15 20:51:45 +0200

    Rejecting edits of reserved tickets

--- 1.mini-microservice-app-grider/ticketing/tickets/src/routes/__test__/update.test.ts
+++ 1.mini-microservice-app-grider/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
 import { natsWrapper } from "../../nats-wrapper";
 
 it.skip("returns a 404 if the provided id does not exist", async () => {
@@ -112,4 +113,26 @@ it.skip("publishes an event", async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it.skip("rejects updates if the ticket is reserved", async () => {
+  const cookie = (global as any).signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "asdas",
+      price: 20,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "new title", price: 100 })
+    .expect(400);
 });
