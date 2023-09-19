c8f7cbeecd39b1ac1cd4a3ea7f0016897363c37a
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-19 21:01:21 +0200

    Add expiration service

--- 1.mini-microservice-app-grider/ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ 1.mini-microservice-app-grider/ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -14,10 +14,10 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     await expirationQueue.add(
       {
         orderId: data.id,
-      },
-      {
-        delay: delay,
       }
+      // {
+      //   delay: delay,
+      // }
     );
 
     msg.ack();
