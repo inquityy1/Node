1f3613c7e19ab2faf7dc36376357c7835d7f7b1c
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-26 18:24:19 +0200

    Validating order payment

--- 1.mini-microservice-app-grider/ticketing/payments/src/routes/new.ts
+++ 1.mini-microservice-app-grider/ticketing/payments/src/routes/new.ts
@@ -5,6 +5,8 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
 } from "@myfirstnpmhere/common";
 import { Order } from "../models/order";
 
@@ -15,7 +17,21 @@ router.post(
   requireAuth,
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for an cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
