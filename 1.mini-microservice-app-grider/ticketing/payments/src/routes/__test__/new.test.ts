8aeba51886cd13c8285f38e693cee62e5b98de69
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-27 19:30:12 +0200

    Testing payment creation

--- 1.mini-microservice-app-grider/ticketing/payments/src/routes/__test__/new.test.ts
+++ 1.mini-microservice-app-grider/ticketing/payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@myfirstnpmhere/common";
 import { stripe } from "../../stripe";
+import { Payment } from "../../models/payment";
 
 it.skip("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
@@ -85,4 +86,10 @@ it.skip("returns a 201 with valid inputs", async () => {
 
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual("usd");
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+  expect(payment).not.toBeNull();
 });
