8c8cf8be5cc003bff197d3df6f87c908382c2119
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-27 18:58:00 +0200

    Mocked stripe client

--- 1.mini-microservice-app-grider/ticketing/payments/src/routes/__test__/new.test.ts
+++ 1.mini-microservice-app-grider/ticketing/payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@myfirstnpmhere/common";
+import { stripe } from "../../stripe";
 
 jest.mock("../../stripe");
 
@@ -58,7 +59,7 @@ it.skip("returns a 400 when purchasing a cancelled order", async () => {
     .expect(400);
 });
 
-it("returns a 204 with valid inputs", async () => {
+it.skip("returns a 204 with valid inputs", async () => {
   const userId = new mongoose.Types.ObjectId().toHexString();
   const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
@@ -75,5 +76,11 @@ it("returns a 204 with valid inputs", async () => {
     .send({
       token: "tok_visa",
       orderId: order.id,
-    });
+    })
+    .expect(201);
+
+  const chargeOprions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOprions.source).toEqual("tok_visa");
+  expect(chargeOprions.amount).toEqual(20 * 100);
+  expect(chargeOprions.currency).toEqual("usd");
 });
