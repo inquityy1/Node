0a339fa5c87152f14dd7d97d271fc5b1c7cb1b0c
inquityy1 <nebojsanikolic12350@yahoo.com>
2023-09-29 18:25:17 +0200

    Configuring stripe

--- 1.mini-microservice-app-grider/ticketing/client/pages/orders/[orderId].js
+++ 1.mini-microservice-app-grider/ticketing/client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -21,7 +22,17 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51NueSvGpPmWXGy7tKv7NVQgdHZNXVuRMEKMR1yHZpGSOV32ObEFQV5zOs2kFJR5cJ9KNaMCgqbGJJqY3pT4aEdex00pJfOVEMa"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
